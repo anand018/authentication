dde845af10ef244f091dcc844706437977672c80
Anand Pratap Singh <42322585+anand018@users.noreply.github.com>
2023-11-26 10:46:01 +0530

    Add files via upload
    CORS config

---
+++ src/main/java/com/simlearn/authentication/config/ApplicationConfig.java
@@ -0,0 +1,24 @@
+package com.simlearn.authentication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // Add the origin of your React app
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
